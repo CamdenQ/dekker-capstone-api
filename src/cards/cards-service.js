44987d0be1bd39c48d551ebc43e4cb4a5a2f5eda
Camden Miller <camdenquin@gmail.com>
2020-08-18 01:23:10 -0400

    removed unneeded experimental code

--- src/cards/cards-service.js
+++ src/cards/cards-service.js
@@ -4,21 +4,10 @@ const knex = require('../../db/knex');
 
 const CardsService = {
   getAllCards(page) {
-    return knex('cards').select('*').paginate({
+    return knex('cards').paginate({
       perPage: 20,
       currentPage: page,
     });
-  },
-
-  getCardsByColor(color) {
-    console.log(`COLOR = ${color}`);
-    console.log('Color variable type = ' + typeof color);
-    const colorSQL = `'{"${color}"}`;
-    return knex.raw(`SELECT * FROM cards WHERE colors @> (??)`, colorSQL);
-    // .paginate({
-    //   perPage: 20,
-    //   currentPage: page,
-    // });
   },
 };
 
